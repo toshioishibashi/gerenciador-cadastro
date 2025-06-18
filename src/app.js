a81e9d9292579f90b2766df14d8c17c9ea5060a9
rodrigo_ishibashi@hotmail.com <rodrigo_ishibashi@hotmail.com>
2025-06-18 15:46:50 -0300

    Mesclando histórico local com remoto

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import tutorRoutes from './routes/tutorRoutes.js';
 import petRoutes from './routes/petRoutes.js';
-<<<<<<< HEAD
 import vacinaRoutes from './routes/vacinaRoutes.js';
 import veterinarioRoutes from './routes/veterinarioRoutes.js';
 import setupSwagger from './Swagger/swagger.js';
@@ -11,21 +10,13 @@ import setupSwagger from './Swagger/swagger.js';
 dotenv.config();
 const app = express();
 setupSwagger(app);
-=======
-
-dotenv.config();
-const app = express();
->>>>>>> 26140451d846ffd738b33d0a35db12fabab1aaef
 
 app.use(express.json());
 
 app.use(tutorRoutes);
 app.use(petRoutes);
-<<<<<<< HEAD
 app.use('/vacinas', vacinaRoutes);
 app.use('/veterinarios', veterinarioRoutes);
-=======
->>>>>>> 26140451d846ffd738b33d0a35db12fabab1aaef
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
